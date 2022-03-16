7993609d297d38310376a3671fc4588b1f6a0bfe
JUNYOUNG31 <roywnsdud31@gmail.com>
2022-03-16 13:12:43 +0900

    [FE] Feat: 유니티 맵 생성

---
+++ maze map/Assets/Script/StartGame.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartGame : MonoBehaviour
+{
+    public GameObject StartBtn;
+    bool timeActive = false;
+    public float CountTime;
+    public Text text_Timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "Witch3")
+        {
+            StartBtn.SetActive(false);
+            timeActive = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (StartBtn.activeSelf == false)
+        {
+            if (timeActive)
+            {
+                CountTime += Time.deltaTime;
+                text_Timer.text = CountTime.ToString("F2");
+            }
+        }
+    }
+}

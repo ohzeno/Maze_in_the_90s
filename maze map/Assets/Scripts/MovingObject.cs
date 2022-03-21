d17a97279cfc86be1d5e4f1c6da472b77a300fff
조은누리 <jounuli@naver.com>
2022-03-21 15:22:49 +0900

    [BE] Feat: Firebase Auth - Front UI 연결
    Former-commit-id: 66f139da1d36ed7a1c0189a33ad50133e18cb65a

---
+++ maze map/Assets/Scripts/MovingObject.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour
+{
+
+    public static MovingObject instance;
+
+    private BoxCollider2D boxCollider;
+    public LayerMask layerMask;
+
+    public float speed;
+    public int walkCount;
+    private int currentWalkCount;
+
+    private Vector3 vector;
+    private Animator animator;
+
+    public float runSpeed;
+    private float applyRunSpeed;
+    private bool applyRunFlag = false;
+    private bool canMove = true;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    IEnumerator MoveCoroutine()
+    {
+        while (Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0)
+        {
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                applyRunSpeed = runSpeed;
+                applyRunFlag = true;
+            }
+            else
+            {
+                applyRunSpeed = 0;
+                applyRunFlag = false;
+            }
+
+
+            vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
+
+            if (vector.x != 0)
+                vector.y = 0;
+
+
+            animator.SetFloat("DirX", vector.x);
+            animator.SetFloat("DirY", vector.y);
+
+            RaycastHit2D hit;
+            Vector2 start = transform.position;
+            Vector2 end = start + new Vector2(vector.x * speed * walkCount, vector.y * speed * walkCount);
+
+            boxCollider.enabled = false;
+            hit = Physics2D.Linecast(start, end, layerMask);
+            boxCollider.enabled = true;
+
+            if (hit.transform != null)
+                break;
+
+            animator.SetBool("Walking", true);
+
+            while (currentWalkCount < walkCount)
+            {
+
+                transform.Translate(vector.x * (speed + applyRunSpeed), vector.y * (speed + applyRunSpeed), 0);
+                if (applyRunFlag)
+                    currentWalkCount++;
+                currentWalkCount++;
+                yield return new WaitForSeconds(0.01f);
+            }
+            currentWalkCount = 0;
+
+        }
+        animator.SetBool("Walking", false);
+        canMove = true;
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (canMove)
+        {
+            if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+            {
+                canMove = false;
+                StartCoroutine(MoveCoroutine());
+            }
+        }
+
+    }
+}

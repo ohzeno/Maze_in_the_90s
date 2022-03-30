a638d309b8aaac15d767bd609c641b9540c4d8d5
ohzeno <blackroseaaa3@gmail.com>
2022-03-30 16:28:46 +0900

    [BE] Update: 리액트-장고 통신 연습

---
+++ react_django_test/django_blob/recog/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = 'recog'
+
+urlpatterns = [
+    path('', views.processing),
+    path("upload", views.uploadFile, name = "uploadFile"),
+    path('blob', views.getBlob, name="getBlob"),
+    path('detect/<str:user_name>/', views.detect, name="detect"),
+    path('detect/<str:user_name>/control', views.getControl, name = "getControl"),
+    path('detect/<str:user_name>/delete', views.getControl, name = "getControl"),
+]
+
+if settings.DEBUG: 
+    urlpatterns += static(
+        settings.MEDIA_URL, 
+        document_root = settings.MEDIA_ROOT
+    )
--- react_django_test/react_webcam/src/App.js
+++ react_django_test/react_webcam/src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
 import Webcam from "react-webcam";
-// import jpeg from "jpeg-js";
-// import pako from "pako";
 import axios from "axios";
-// import decode from "image-decode";
 import inkjet from "inkjet";
 import FormData from "form-data";
 
@@ -27,66 +24,55 @@ const WebcamStreamCapture = () => {
   const handleObserveClick = async () => {
     await sleep(300);
     setInterval(async () => {
+      /* ========================================================
+       * convert: stream -> image -> blob
+       * ======================================================== */
       const stream = new MediaStream(webcamRef.current.stream);
       const track = stream.getVideoTracks()[0];
       const image = new ImageCapture(track);
       const blob = await image.takePhoto(); // Blob(size=~, type=image/jpeg)
-      // console.log("blob", blob);
-      const buffer = await blob.arrayBuffer();
-      // console.log("buffer", buffer);
-      const compressed = new Uint8Array(buffer);
-      const decompressed = await decode(compressed);
-      // console.log("decompressed", decompressed);
-      // console.log("size", decompressed.width * decompressed.height * 4);
+      // console.log({ blob });
+      const imgSrc = URL.createObjectURL(blob); // current image's imgSrc used in <img src={imgSrc}>
+      setImageSrc(imgSrc); // blob to output image // response data --> blob is also ok
+      // console.log({ imgSrc });
 
-      // const { data } = await axios.put(
-      //   `http://115.22.130.177:3000/recog/blob`,
-      //   blob,
-      //   {
-      //     headers: {
-      //       "Content-Type": "arraybuffer",
-      //     },
-      //   }
+      // const buffer = await blob.arrayBuffer();
+      // const compressed = new Uint8Array(buffer);
+      // const decompressed = await decode(compressed); // we dont need to decompress here
+
+      /* ========================================================
+       * send: blob -> server -> data -> binary
+       * ======================================================== */
+      const hosan = "http://hosan.iptime.org:8000";
+      const zeno = "http://127.0.0.1:8000";
+      const hosan_sub = "/webcam/upload";
+      const zeno_sub = "/recog/upload";
+      const api = axios.create({ baseURL: zeno });
+      const form = new FormData();
+      form.append("image", blob);
+      const { data } = await api.post(zeno_sub, form, {
+        responseType: "blob",
+      });
+      const arr = await data.arrayBuffer();
+      console.log({ arr });
+      const binary = new Uint8Array(arr); // Uint8Array height*width*4 elem
+      console.log({ binary });
+
+      /* ========================================================
+       * convert: binary -> image -> imageSrc
+       * ref: https://stackoverflow.com/questions/50620821/uint8array-to-image-in-javascript
+       *      https://seunggabi.tistory.com/entry/JS-convert-bytes-array-to-image
+       *      https://stackoverflow.com/questions/4564119/how-to-convert-a-byte-array-into-an-image
+       *      https://mineeeee.tistory.com/12#:~:text=PNG%ED%8C%8C%EC%9D%BC%EA%B5%AC%EC%A1%B0%EB%8A%94%20%ED%8C%8C%EC%9D%BC,%EC%A7%91%ED%95%A9%EC%9C%BC%EB%A1%9C%20%EA%B5%AC%EC%84%B1%EB%90%98%EC%96%B4%20%EC%9E%88%EC%8A%B5%EB%8B%88%EB%8B%A4.&text=PNG%20%ED%8C%8C%EC%9D%BC%20%EC%8B%9C%EA%B7%B8%EB%8B%88%EC%B2%98%EB%8A%94%2089,ASCII%20CODE%EB%A1%9C%20PNG%EC%9E%85%EB%8B%88%EB%8B%A4!
+       * 서버에서 받은 이미지를 띄우고 싶다면, w*h*4 개의 uint8 데이터를 imgSrc로 변환해서 <img src={}> 에 넣으면됨.
+       * 동작하는 케이스를 찾아서 사용하면 될듯
+       * ======================================================== */
+      // const imgSrc = URL.createObjectURL(
+      //   new Blob([binary], { type: "image/png" })
       // );
-
-      // Uint8Array to arrayBuffer: uarray.slice(0, uarray.byteOffset): https://stackoverflow.com/questions/37228285/uint8array-to-arraybuffer
-      // arrayBuffer to Uint8Array: new Uint8Array(arrayBuffer)
-
-      // const form = new FormData();
-      // form.append("blob", blob, "blob");
-      const { data } = axios
-        .post(
-          // `http://115.22.130.177:3000/recog/upload`,
-          `http://127.0.0.1:8000/recog/upload`,
-          decompressed,
-          // compressed,
-          // blob,
-          {
-            headers: {
-              // "Content-Type": "image/png",
-              // "Content-Type": "application/x-www-form-urlencoded",
-              "Content-Type": "multipart/form-data",
-              // "Content-Type": " application/octet-stream",
-              // filename: "what",
-            },
-          }
-        )
-        .then((res) => {
-          console.log("RESPONSE RECEIVED: ", res);
-        })
-        .catch((err) => {
-          console.log("AXIOS ERROR: ", err);
-        });
-
-      // const { data } = axios.post(`http://115.22.130.177:3000/recog/blob`, {
-      //   width: decompressed.width,
-      //   height: decompressed.height,
-      //   data: decompressed.data,
-      // });
-
-      console.log("data", data);
-
-      setImageSrc(URL.createObjectURL(blob)); // blob to output image // response data --> blob is also ok
+      // const imgSrc = "data:image/png;base64," + btoa(binary);
+      // console.log({ imgSrc });
+      // setImageSrc(imgSrc);
     }, 5000);
   };
 
